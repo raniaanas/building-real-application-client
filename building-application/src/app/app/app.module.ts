43f7ab978072d98a19888c918cff6b157eee6b90
raniaanas <raniaanas994@gmail.com>
2024-09-30 10:45:34 +0300

    add first component

---
+++ building-application/src/app/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PropertyCardComponent } from '../property/property-card/property-card.component';
+
+
+
+@NgModule({
+  declarations: [
+    PropertyCardComponent,
+
+
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppModule { }
